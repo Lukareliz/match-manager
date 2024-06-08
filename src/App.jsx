cbf7abd0eeb2a3772b831c896edf9e20c1353383
= <lyulucca@gmail.com>
2024-06-08 16:20:11 -0300

    feat: timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import HeaderMenu from './components/HeaderMenu/Index'
 import ScoreBoard from './components/ScoreBoard/Index'
 
 import './App.css'
 
 function App() {
+  //SCORE
   const [countRed, setCountRed] = useState(0)
   const [countBlue, setCountBlue] = useState(0)
 
   const resetCounts = () => {
     setCountRed(0);
     setCountBlue(0);
+    setTime(0);
+    setIsRunning(false);
   }
+
+  const controlTimer = () => {
+    if(isRunning == true) {
+      setIsRunning(false)
+    } else {
+      setIsRunning(true)
+    }
+  }
+
+  //TIMER
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef(null);
+
+  const formatTime = (seconds) => {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
+    return `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+  };
+
+  useEffect(() => {
+    if (isRunning) {
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(timerRef.current);
+    }
+    return () => clearInterval(timerRef.current);
+  }, [isRunning]);
 
   return (
     <>
-      <HeaderMenu resetCounts={resetCounts}/>
+      <HeaderMenu resetCounts={resetCounts} controlTimer={controlTimer} isRunning={isRunning} />
+      <p className='timer'>{formatTime(time)}</p>
       <main>
-        <ScoreBoard countRed={countRed} setCountRed={setCountRed} countBlue={countBlue} setCountBlue={setCountBlue}/>
+        <ScoreBoard countRed={countRed} setCountRed={setCountRed} countBlue={countBlue} setCountBlue={setCountBlue} />
       </main>
     </>
   )
--- src/components/HeaderMenu/Index.jsx
+++ src/components/HeaderMenu/Index.jsx
@@ -1,15 +1,18 @@
 import { IoRefresh } from "react-icons/io5";
+import { IoPlay } from "react-icons/io5";
 import { IoPauseOutline } from "react-icons/io5";
 import { IoShuffleOutline } from "react-icons/io5";
 
 import '../HeaderMenu/headerMenu.css'
 
-const HeaderMenu = ({ resetCounts }) => {
+const HeaderMenu = ({ resetCounts, controlTimer, isRunning }) => {
   return (
     <header className="header-menu">
-      <button className="header-menu-button"><IoRefresh className="header-menu-icon" onClick={resetCounts}/></button>
-      <button className="header-menu-button"><IoPauseOutline className="header-menu-icon"/></button>
-      <button className="header-menu-button"><IoShuffleOutline className="header-menu-icon"/></button>
+      <button className="header-menu-button" onClick={resetCounts}><IoRefresh className="header-menu-icon" /></button>
+      <button className="header-menu-button" onClick={controlTimer}>
+        {isRunning ? <IoPauseOutline className="header-menu-icon" /> : <IoPlay className="header-menu-icon" />}
+      </button>
+      <button className="header-menu-button"><IoShuffleOutline className="header-menu-icon" /></button>
     </header>
   )
 }
