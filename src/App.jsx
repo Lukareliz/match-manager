42dbccf1750c3c8b2355bc44e6624706335adad3
= <lyulucca@gmail.com>
2024-06-08 01:12:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // onClick={() => setCount((count) => count + 1)}
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
